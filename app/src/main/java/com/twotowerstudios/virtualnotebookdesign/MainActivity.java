79409c25c7444c9ec29a8e547c7ec4d46487be03
coldblade2000 <diegogranada7@gmail.com>
2016-10-07 11:34:23 -0500

    began implementation of MaterialDrawer Library (https://github.com/mikepenz/MaterialDrawer)

--- app/src/main/java/com/twotowerstudios/virtualnotebookdesign/MainActivity.java
+++ app/src/main/java/com/twotowerstudios/virtualnotebookdesign/MainActivity.java
@@ -1,13 +1,17 @@
 package com.twotowerstudios.virtualnotebookdesign;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.mikepenz.materialdrawer.DrawerBuilder;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Implement https://github.com/mikepenz/MaterialDrawer
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new DrawerBuilder().withActivity(this).build();
     }
 }

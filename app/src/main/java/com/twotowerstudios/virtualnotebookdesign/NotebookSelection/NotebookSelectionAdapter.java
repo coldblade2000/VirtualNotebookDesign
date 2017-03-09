276bd66938009bb63f846261712cc6d18f6813ba
coldblade2000 <diegogranada7@gmail.com>
2017-03-09 11:50:32 -0500

    UNTESTED * deleting full notebooks and deleting its images

--- app/src/main/java/com/twotowerstudios/virtualnotebookdesign/NotebookSelection/NotebookSelectionAdapter.java
+++ app/src/main/java/com/twotowerstudios/virtualnotebookdesign/NotebookSelection/NotebookSelectionAdapter.java
@@ -3,21 +3,28 @@ package com.twotowerstudios.virtualnotebookdesign.NotebookSelection;
 import android.app.Activity;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.os.Environment;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.text.Html;
 import android.text.format.DateUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.twotowerstudios.virtualnotebookdesign.Misc.Helpers;
+import com.twotowerstudios.virtualnotebookdesign.Objects.ChildBase;
 import com.twotowerstudios.virtualnotebookdesign.Objects.Notebook;
+import com.twotowerstudios.virtualnotebookdesign.Objects.Page;
 import com.twotowerstudios.virtualnotebookdesign.R;
 
+import java.io.File;
 import java.util.ArrayList;
+
 
 /**
  * Created by Panther II on 22/10/2016.
@@ -56,63 +63,103 @@ public class NotebookSelectionAdapter extends RecyclerView.Adapter<NotebookSelec
 	}
 	@Override
 	public NotebookSelectionAdapter.ViewHolder onCreateViewHolder (ViewGroup parent, int viewType){
-	View v = LayoutInflater.from(parent.getContext())
-	.inflate(R.layout.notebookselectioncard, parent, false);
-	return new ViewHolder(v);
+		View v = LayoutInflater.from(parent.getContext())
+				.inflate(R.layout.notebookselectioncard, parent, false);
+		return new ViewHolder(v);
 	}
 
 
-	 @Override
-	 public void onBindViewHolder(final ViewHolder holder, int position){
-	 	 final Notebook notebookSelection = notebookList.get(position);
-		 final int position2 = position;
-	 	 holder.tvCardNameSel.setText(""+notebookSelection.getName());
-	 	 holder.tvCardSub.setText("Last modified: "+
-				 DateUtils.getRelativeTimeSpanString(notebookSelection.getLastModified(), Helpers.getCurrentTimeInMillis(), DateUtils.SECOND_IN_MILLIS));
-	 	 holder.tvPageCount.setText("" +notebookSelection.getNumberOfPages() + " Pages");
-	 	 holder.ivCardImage.setColorFilter(notebookSelection.getColor());
-		 holder.card.setOnClickListener(new View.OnClickListener() {
-			 @Override
-			 public void onClick(View view) {
-				 Interface.openNotebookActivity(position2);
-			 }
-		 });
-		 holder.card.setOnLongClickListener(new View.OnLongClickListener() {
-			 @Override
-			 public boolean onLongClick(View v) {
-				 new AlertDialog.Builder(mActivity)
-						 .setTitle("Do you want to delete this image?")
-						 .setPositiveButton("Delete", new DialogInterface.OnClickListener() {
-							 @Override
-							 public void onClick(DialogInterface dialog, int which) {
-								 if (notebookList.get(holder.getAdapterPosition()).getNumberOfPages()<=0) {
-									 notebookList.remove(holder.getAdapterPosition());
-									 Helpers.writeListToFile(context, notebookList);
-									 notifyItemRemoved(holder.getAdapterPosition());
-								 } else {
+	@Override
+	public void onBindViewHolder(final ViewHolder holder, int position){
+		final Notebook notebookSelection = notebookList.get(position);
+		final int position2 = position;
+		holder.tvCardNameSel.setText(""+notebookSelection.getName());
+		holder.tvCardSub.setText("Last modified: "+
+				DateUtils.getRelativeTimeSpanString(notebookSelection.getLastModified(), Helpers.getCurrentTimeInMillis(), DateUtils.SECOND_IN_MILLIS));
+		holder.tvPageCount.setText("" +notebookSelection.getNumberOfPages() + " Pages");
+		holder.ivCardImage.setColorFilter(notebookSelection.getColor());
+		holder.card.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				Interface.openNotebookActivity(position2);
+			}
+		});
+		holder.card.setOnLongClickListener(new View.OnLongClickListener() {
+			@Override
+			public boolean onLongClick(View v) {
+				if (notebookList.get(holder.getAdapterPosition()).getNumberOfPages()<=0) {
+					//make this delete the pictures
+					new AlertDialog.Builder(mActivity)
+							.setTitle("Do you want to delete this notebook?")
+							.setMessage("Every photo and item will be deleted")
+							.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+								@Override
+								public void onClick(DialogInterface dialog, int which) {
 
-								 }
+									notebookList.remove(holder.getAdapterPosition());
+									Helpers.writeListToFile(context, notebookList);
+									notifyItemRemoved(holder.getAdapterPosition());
+								}
+							})
 
-							 }
-						 })
+							.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+								@Override
+								public void onClick(DialogInterface dialog, int which) {
 
-						 .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-							 @Override
-							 public void onClick(DialogInterface dialog, int which) {
+								}
+							}).show();
+				}else{
+					final EditText edittext = new EditText(context);
+					AlertDialog.Builder dialog = new AlertDialog.Builder(mActivity);
+					dialog.setTitle("Do you want to delete this notebook?");
+					if(android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+						dialog.setMessage(Html.fromHtml("You will have to type out the exact name of the notebook to confirm." + "<br>" + "<b>" + notebookSelection.getName() + "</b>", Html.FROM_HTML_MODE_LEGACY));
+					}else {
+						dialog.setMessage(Html.fromHtml("You will have to type out the exact name of the notebook to confirm." + "<br>" + "<b>" + notebookSelection.getName() + "</b>"));
+					}dialog.setView(edittext);
+					dialog.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+							for(Page a: notebookSelection.getPages()){
+								for (ChildBase b: a.getContent()) {
+									if(b.getChildType()==1){
+										File fdelete = new File(context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)+b.getUri().getPath());
+										if (fdelete.exists()) {
+											if (fdelete.delete()) {
+												System.out.println("file Deleted :" + context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)+b.getUri().getPath());
+											} else {
+												System.out.println("file not Deleted :" + context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)+b.getUri().getPath());
+											}
+										}
 
-							 }
-						 }).show();
+									}
+								}
+							}
+							notebookList.remove(holder.getAdapterPosition());
+							Helpers.writeListToFile(context, notebookList);
+							notifyItemRemoved(holder.getAdapterPosition());
 
-				 return false;
-			 }
-		 });
-	 }
-	 @Override
-	 public int getItemCount() {
-	 	return notebookList.size();
-	 }
+						}
+					});
+
+					dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+
+						}
+					}).show();
+
+				}
+				return false;
+			}
+		});
+	}
+	@Override
+	public int getItemCount() {
+		return notebookList.size();
+	}
 	/**@Override
 	public void refreshData() {
-		notifyDataSetChanged();
+	notifyDataSetChanged();
 	}*/
 }
